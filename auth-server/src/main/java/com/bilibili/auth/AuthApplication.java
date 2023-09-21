c74b2f0a2c33ce598631171730021e2ecd229497
Silvery <1364795855@qq.com>
2023-09-21 23:14:44 +0800

    登陆重定向

--- auth-server/src/main/java/com/bilibili/auth/AuthApplication.java
+++ auth-server/src/main/java/com/bilibili/auth/AuthApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.bilibili.commons", "com.bilibili.auth"})
 @MapperScan("com.bilibili.commons.mapper")
 public class AuthApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class);
     }
